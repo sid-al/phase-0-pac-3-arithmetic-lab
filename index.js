daa007f56a2f1f1c99379d71ecfc8f7586e59423
sid-al <sendplusx@gmail.com>
2022-06-05 09:37:41 +0300

    Done.

---
+++ index.js
@@ -0,0 +1,42 @@
+function add(a, b){
+    return a + b;
+
+}
+
+function subtract(a,b){
+    return a - b;
+}
+
+
+function multiply(a,b){
+    return a * b;
+}
+
+
+function divide(a,b){
+    return a / b;
+}
+
+function increment(n){
+
+    return n+1;
+}
+
+function decrement(n){
+
+    return n-1;
+}
+
+function makeInt(n){
+    return parseInt(n);
+}
+
+function makeInt(n){
+    return parseFloat(n);
+}
+
+
+
+function preserveDecimal(n){
+    return parseFloat(n);
+}
